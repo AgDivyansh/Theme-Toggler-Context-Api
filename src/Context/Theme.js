30e6099663a657ba021eba8be3442f8d26e3d8c7
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-05-14 12:43:59 +0530

    first commit

---
+++ src/Context/Theme.js
@@ -0,0 +1,17 @@
+import { createContext, useContext } from "react";
+
+
+export const ThemeContext = createContext({
+    themeMode: "Light",
+    darkTheme: () => {},
+    lightTheme : () => {}
+    
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+
+export default function useTheme ()
+{
+    return useContext (ThemeContext) 
+}
