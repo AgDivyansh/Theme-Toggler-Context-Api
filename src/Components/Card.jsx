850a301f1b47138858af0918f749bbfea14d2e00
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2024-05-16 12:47:49 +0530

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -70,14 +70,7 @@ export default function Card() {
             4.0
           </span>
         </div>
-        <div className="flex items-center justify-between">
-          <a
-            href="/"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Add to cart
-          </a>
-        </div>
+        
       </div>
     </div>
   );
